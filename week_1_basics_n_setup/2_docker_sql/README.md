08333a72876ef50d161a1d139fa7bd6c1efce535
Shivani Padal <spadal@ariasystems.com>
2023-01-25 11:04:04 +0530

    Ingest the data of green_trips and yellow_trips in two tables

--- week_1_basics_n_setup/2_docker_sql/README.md
+++ week_1_basics_n_setup/2_docker_sql/README.md
@@ -215,6 +215,21 @@ docker run -it \
     --table_name=yellow_taxi_trips \
     --url=${URL}
 ```
+```bash 
+
+Inserting all table data at a time 
+
+docker run -it \
+  --network=2_docker_sql_default \
+  taxi_ingest:v001 \
+    --user root \
+    --password root \
+    --host pgdatabase \
+    --port 5432 \
+    --db ny_taxi \
+    --table_names yellow_taxi_trips  green_taxi_trips   zones \
+    --urls https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_2021-01.csv.gz    https://github.com/DataTalksClub/nyc-tlc-data/releases/download/green/green_tripdata_2019-01.csv.gz    https://github.com/DataTalksClub/nyc-tlc-data/releases/download/misc/taxi_zone_lookup.csv
+```
 
 ### Docker-Compose 
 
--- week_1_basics_n_setup/2_docker_sql/ingest_data.py
+++ week_1_basics_n_setup/2_docker_sql/ingest_data.py
@@ -16,51 +16,65 @@ def main(params):
     host = params.host 
     port = params.port 
     db = params.db
-    table_name = params.table_name
-    url = params.url
+    table_names = params.table_names
+    urls = params.urls
     
     # the backup files are gzipped, and it's important to keep the correct extension
     # for pandas to be able to open the file
-    if url.endswith('.csv.gz'):
-        csv_name = 'output.csv.gz'
-    else:
-        csv_name = 'output.csv'
 
-    os.system(f"wget {url} -O {csv_name}")
 
     engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
-
-    df_iter = pd.read_csv(csv_name, iterator=True, chunksize=100000)
-
-    df = next(df_iter)
-
-    df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
-    df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
-
-    df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
-
-    df.to_sql(name=table_name, con=engine, if_exists='append')
-
-
-    while True: 
-
-        try:
-            t_start = time()
-            
-            df = next(df_iter)
-
+    for table_name,url in list(zip(table_names,urls)):
+        print(f'Table name is {table_name}, url is {url}')
+        
+        if url.endswith('.csv.gz'):
+            csv_name = table_name + '.csv.gz'
+        else:
+            csv_name = table_name + '.csv'
+        
+        os.system(f"wget {url} -O {csv_name}")
+        
+        if (table_name.endswith('trips')) :
+             df_iter = pd.read_csv(csv_name, iterator=True, chunksize=100000)
+        else:
+             df_iter=pd.read_csv(csv_name, iterator=True, chunksize=500)
+        
+        df = next(df_iter)
+        
+        if (table_name == 'yellow_taxi_trips'):
             df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
             df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
-
-            df.to_sql(name=table_name, con=engine, if_exists='append')
-
-            t_end = time()
-
-            print('inserted another chunk, took %.3f second' % (t_end - t_start))
-
-        except StopIteration:
-            print("Finished ingesting data into the postgres database")
-            break
+        elif (table_name == 'green_taxi_trips'):
+            df.lpep_pickup_datetime = pd.to_datetime(df.lpep_pickup_datetime)
+            df.lpep_dropoff_datetime = pd.to_datetime(df.lpep_dropoff_datetime)
+        
+        df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
+        
+        df.to_sql(name=table_name, con=engine, if_exists='append')
+        
+        
+        while True: 
+        
+            try:
+                t_start = time()
+                
+                df = next(df_iter)
+                if (table_name == 'yellow_taxi_trips') :
+                    df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+                    df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+                elif (table_name == 'green_taxi_trips') :
+                    df.lpep_pickup_datetime = pd.to_datetime(df.lpep_pickup_datetime)
+                    df.lpep_dropoff_datetime = pd.to_datetime(df.lpep_dropoff_datetime)
+                
+                df.to_sql(name=table_name, con=engine, if_exists='append')
+        
+                t_end = time()
+        
+                print('inserted another chunk, took %.3f second' % (t_end - t_start))
+        
+            except StopIteration:
+                print("Finished ingesting data into the postgres database")
+                break
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Ingest CSV data to Postgres')
@@ -70,8 +84,8 @@ if __name__ == '__main__':
     parser.add_argument('--host', required=True, help='host for postgres')
     parser.add_argument('--port', required=True, help='port for postgres')
     parser.add_argument('--db', required=True, help='database name for postgres')
-    parser.add_argument('--table_name', required=True, help='name of the table where we will write the results to')
-    parser.add_argument('--url', required=True, help='url of the csv file')
+    parser.add_argument('--table_names', required=True, help='name of the table where we will write the results to',nargs=3)
+    parser.add_argument('--urls', required=True, help='url of the csv file',nargs=3)
 
     args = parser.parse_args()
 
